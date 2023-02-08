1c8d1a88c15425cd8505a0a5140f41403728facf
Michael Pavelchuk <error2268@gmail.com>
2023-02-08 19:48:35 +0200

    get posts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  const getUserPosts = async (userId) => {
+    const response = await fetch(
+      `https://mastodon.world/api/v1/accounts/${userId}/statuses`,
+      {
+        headers: {
+          Authorization: `Bearer rMF_xZ6EmkT6MNvsjI7-TsvupzNoJuDd5G5utm8dSFk`,
+        },
+      }
+    );
+    const posts = await response.json();
+    setPosts(posts);
+  };
+
+  const getUserId = async () => {
+    const response = await fetch(
+      `https://mastodon.world/api/v1/accounts/search?q=MichaelBaller`,
+      {
+        headers: {
+          Authorization: `Bearer rMF_xZ6EmkT6MNvsjI7-TsvupzNoJuDd5G5utm8dSFk`,
+        },
+      }
+    );
+    const user = await response.json();
+    getUserPosts(user && user[0].id);
+  };
+
+  useEffect(() => {
+    getUserId();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {posts.length > 0 &&
+        posts.map(({ account, content }) => {
+          return (
+            <div className="card">
+              <h1>{account.username}</h1>
+              <img src={account.avatar} alt={account.username} />
+              <p>{content}</p>
+            </div>
+          );
+        })}
     </div>
   );
 }
